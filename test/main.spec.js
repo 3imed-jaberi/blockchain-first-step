86e4936a9f707d7ad82f3c55d84f923b2fd783cf
imed jaberi <imed_jebari@hotmail.fr>
2019-07-05 15:19:49 +0200

    Block 🎲 unit test using mocha ☕️ & chai 🍵 ..

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,14 +1,29 @@
 
 'use strict';
 
+require('dotenv').config();
+
 const expect = require('chai').expect;
-const author = require('../src/main');
+const Block = require('../src/Block');
 
-describe('intial unit test using ` mocha ☕️ ` & ` chai 🍵 ` 👻 ..', () => {
 
-  it('Checkout author is `Imed Jaberi` 🚀 ', () => {
-    // like 5 === 5 ..
-    expect(author).to.equal("Imed Jaberi");
+describe(' `Block 🎲 ` unit test using ` mocha ☕️ ` & ` chai 🍵 ` 👻 ..', () => {
+
+  let data, previousBlock, block;
+
+  beforeEach(() => {
+    data = process.env.AUTHOR;
+    previousBlock = Block.GenerateGenesisBlock();
+    block = Block.MineBlock(previousBlock, data);
+  });
+
+  it('set `data` to match the input', () => {
+    expect(block.data).to.equal(data);
+  });
+
+  it('set the `previousHash` to match the hash of the pevious block', () => {
+    expect(block.previousHash).to.equal(previousBlock.hash);
   });
 
 });
+
