27a7c2c36c1348f8e1431af0116910d1d4871f98
imed jaberi <imed_jebari@hotmail.fr>
2019-07-05 15:01:58 +0200

    right way to generate unique id / timestamp date ✔️ ..

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
+const uuid = require('uuid/v4');
 const Block = require('./src/Block');
 
 // const block = new Block('100','2007-07-31','last-hash-demo','current-hash-demo','jawher jaberi <brother> ..');
 
-// console.log(block.toString());
+const block = new Block(uuid().substring(0,5),Date.now(),'last-hash-demo','current-hash-demo','jawher jaberi <brother> ..');
 
-console.log(Block.GenerateGenesisBlock().toString());
+console.log(block.toString());
+
+// const block = new Block(uuid().substring(0,5),Date.now(),'last-hash-demo','current-hash-demo','jawher jaberi <brother> ..');
