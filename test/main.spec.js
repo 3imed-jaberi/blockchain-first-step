6754cc53d479539316ef0374281393709235f3d2
imed jaberi <imed_jebari@hotmail.fr>
2019-07-05 14:49:46 +0200

    unit testing for merkle tree 🌡 ..

--- test/main.spec.js
+++ test/main.spec.js
@@ -2,13 +2,13 @@
 'use strict';
 
 const expect = require('chai').expect;
-const author = require('../src/main');
+const MerkleRoot = require('../src/main');
 
-describe('intial unit test using ` mocha ☕️ ` & ` chai 🍵 ` 👻 ..', () => {
+describe('Merkle Root unit test using ` mocha ☕️ ` & ` chai 🍵 ` 👻 ..', () => {
 
-  it('Checkout author is `Imed Jaberi` 🚀 ', () => {
+  it('Check out `MerkleRoot` 🚀 ', () => {
     // like 5 === 5 ..
-    expect(author).to.equal("Imed Jaberi");
+    expect(MerkleRoot).to.equal('7ed0ef1f4a0f7e02413d0f19708f9bde561158a5edd44315ea84e0f18ad7bce3');
   });
 
 });
