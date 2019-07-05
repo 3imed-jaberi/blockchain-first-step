2a77fdb1d32a46d709721e3eb3cce8199b2544ea
imed jaberi <imed_jebari@hotmail.fr>
2019-07-05 16:09:23 +0200

    drop all 'index' --> will add in the future when we use storage 💾 ..

--- src/Block.js
+++ src/Block.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 const SHA256 = require('crypto-js/sha256');
-const uuid = require('uuid/v4');
+// const uuid = require('uuid/v4');
 
 class Block {
 
-  constructor(index,timestamp, previousHash, hash, data) {
-    this.index = index;
+  constructor(/* index, */timestamp, previousHash, hash, data) {
+   // this.index = index;
     this.timestamp = timestamp;
     this.previousHash = previousHash;
     this.hash = hash;
@@ -14,7 +14,7 @@ class Block {
   }
 
   toString(){
-    return ` Block N°: ${this.index} -- 
+    return ` Block N°: ${process.env.DEFAULT_INDEX/* this.index */} -- 
     Timestamp     : ${this.timestamp} 
     Previous Hash : ${this.previousHash.substring(0,10)} 
     Hash          : ${this.hash.substring(0,10)} 
@@ -23,21 +23,22 @@ class Block {
   }
 
   static GenerateGenesisBlock() {
-    const { INDEX, TIMESTAMP, PREVIOUS_HASH, HASH, DATA } = process.env ; 
-    return new this (INDEX,TIMESTAMP,PREVIOUS_HASH,HASH,DATA);
+    const { /* INDEX, */ TIMESTAMP, PREVIOUS_HASH, HASH, DATA } = process.env ; 
+    return new this (/* INDEX, */TIMESTAMP,PREVIOUS_HASH,HASH,DATA);
   }
 
   static MineBlock(previousBlock,data){
-    const index = uuid().substring(0,5); 
+    // const index = uuid().substring(0,5); 
     const timestamp = Date.now();
     const previousHash = previousBlock.hash;
-    const hash =  Block.Hash(index,Date.now(),previousHash,data);
+    const hash =  Block.Hash(/* index, */Date.now(),previousHash,data);
 
-    return new this (index,timestamp,previousHash,hash,data);
+    return new this (/* index, */timestamp,previousHash,hash,data);
   }
 
-  Hash (index,timestamp,previousHash,data) { 
-    return SHA256( `${this.index}${this.timestamp}${this.previousHash}${JSON.stringify(this.data)}`).toString();
+  Hash (/* index, */timestamp,previousHash,data) { 
+   // return SHA256( `${this.index}${this.timestamp}${this.previousHash}${JSON.stringify(this.data)}`).toString();
+   return SHA256( `${timestamp}${previousHash}${JSON.stringify(data)}`).toString();
   }
 
 }
